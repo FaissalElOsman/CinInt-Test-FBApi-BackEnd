083ec3fc146a12922229a9b0e7e065a421410736
FaissalElOsman <faissal.elosman@icloud.com>
2017-06-01 19:20:41 +0200

    Saving the token

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
+var token;
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 app.get('/getTest', function(req, res) {
+	var query 			  = require('url').parse(req.url,true).query;
+	token = query.token;
+	console.log("Adding log ...");
   	res.status(200).json({"success": yes}); 
-  	console.log("Adding log ...");
 });
