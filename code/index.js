a2f1c3d19b9735d657e388167743feef73e6642f
FaissalElOsman <faissal.elosman@icloud.com>
2017-06-01 07:46:32 +0200

    First commit

---
+++ code/index.js
@@ -0,0 +1,14 @@
+var express     	= require('express');
+var bodyParser  	= require('body-parser');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/', function(req, res) {
+  	res.status(200).json({"success": true}); 
+});
